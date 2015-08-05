64bc15992252d1590bd3aca259a3018075075a31
Ozzy <ozzytriff@gmail.com>
2015-08-04 22:29:11 -0400

    added keydown/up function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
 	 $('.ryu').mouseenter(function() {
 	 	$('.ryu-still').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
 		$('.ryu-ready').show();
   })
 	 .mouseleave(function() {
     $('.ryu-ready').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
 		$('.ryu-still').show();
   })
 	 .mousedown(function() {
   	playHadouken();
+    $('.ryu-still').hide();
   	$('.ryu-ready').hide();
+    $('.ryu-cool').hide();
   	$('.ryu-throwing').show();
   	$('.hadouken').finish().show().animate(
   		{'left': '1020px'},
@@ -20,9 +26,30 @@ $(document).ready(function() {
   		});
   })
   	.mouseup(function() {
+    $('.ryu-still').hide(); 
   	$('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
   	$('.ryu-ready').show();
-			});
+	});
+  
+    $(document).keydown(function(e) {
+      if (e.keyCode == 88) {
+        $('.ryu-still').hide();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
+        $('.ryu-cool').show();
+      }
+    })
+      .keyup(function(e) {
+      if (e.keyCode == 88) {
+        $('.ryu-still').hide();
+        $('.ryu-cool').hide();
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+      }
+
+})
+
   });
 	
 	function playHadouken() {
